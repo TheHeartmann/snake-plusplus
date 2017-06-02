c563c94a130833beb517395d65e5a5b891aa4305
12inchPlasticToy <marcle15@student.westerdals.no>
2017-06-02 14:13:55 +0200

    First implementation of body, move and grow

--- SDL2_Standardproject/Snake_new.cpp
+++ SDL2_Standardproject/Snake_new.cpp
@@ -4,6 +4,14 @@
 
 #include "Snake_new.h"
 
-Snake_new::Snake_new(int length) {
+unique_ptr<Node> Snake_new::getHead() const { return body.front();}
+unique_ptr<Node> Snake_new::getTail() const { return body.back();}
 
+void Snake_new::move(const unique_ptr<Node> newHeadPosition) {
+    body.push_front(newHeadPosition);
+    body.pop_back();
+}
+
+void Snake_new::grow(const unique_ptr<Node> newHeadPosition) {
+    body.push_front(newHeadPosition);
 }
--- SDL2_Standardproject/Snake_new.h
+++ SDL2_Standardproject/Snake_new.h
@@ -5,21 +5,26 @@
 #ifndef SNAKE_PLUSPLUS_SNAKE_NEW_H
 #define SNAKE_PLUSPLUS_SNAKE_NEW_H
 
-#include <vector>
+#include <list>
 #include <memory>
 #include "Node.h"
 #include "Specifications.h"
 
 using namespace std;
+
 class Snake_new{
 public:
-    Snake_new(int length = Specs.SNAKE_INITIAL_LENGTH);
-    Node getHead();
-    Node getTail;
+    Snake_new(const list<unique_ptr<Node>> _body): body{_body}{}
 
+    unique_ptr<Node> getHead() const;
+    unique_ptr<Node> getTail() const;
+    inline ulong getLength() const { return body.size();}
+
+    void move(const unique_ptr<Node> newHeadPosition);
+    void grow(const unique_ptr<Node> newHeadPosition);
 
 private:
-    vector<unique_ptr<Node>> body;
+    list<unique_ptr<Node>> body;
 };
 
 #endif //SNAKE_PLUSPLUS_SNAKE_NEW_H
