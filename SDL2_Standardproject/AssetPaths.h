176319f2207045a36669ac44346fd7017b5e1b85
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2017-06-08 13:57:47 +0200

    Game works now. Asset paths point out to project folder.

--- SDL2_Standardproject/AssetPaths.h
+++ SDL2_Standardproject/AssetPaths.h
@@ -11,22 +11,22 @@ using namespace std;
 static const struct images {
 	images() {}
 
-	const string snakeHead = "SDL2_Standardproject/Assets/gfx/SnakeHead.png";
-	const string snakeBody = "SDL2_Standardproject/Assets/gfx/SnakeBody.png";
-	const string snakeTail = "SDL2_Standardproject/Assets/gfx/SnakeTail.png";
-	const string apple = "SDL2_Standardproject/Assets/gfx/Apple.png";
-	const string obstacle = "SDL2_Standardproject/Assets/gfx/Obstacle.png";
-	const string wormhole = "SDL2_Standardproject/Assets/gfx/Hole.png";
-	const string background = "SDL2_Standardproject/Assets/gfx/SnakeBoard.png";
+	const string snakeHead = "../SDL2_Standardproject/Assets/gfx/SnakeHead.png";
+	const string snakeBody = "../SDL2_Standardproject/Assets/gfx/SnakeBody.png";
+	const string snakeTail = "../SDL2_Standardproject/Assets/gfx/SnakeTail.png";
+	const string apple = "../SDL2_Standardproject/Assets/gfx/Apple.png";
+	const string obstacle = "../SDL2_Standardproject/Assets/gfx/Obstacle.png";
+	const string wormhole = "../SDL2_Standardproject/Assets/gfx/Hole.png";
+	const string background = "../SDL2_Standardproject/Assets/gfx/SnakeBoard.png";
 } images;
 
 static const struct audio {
 	audio() {}
 
-	const string level = "SDL2_Standardproject/Assets/sfx/musicLoop.wav";
-	const string crunch = "SDL2_Standardproject/Assets/sfx/eating.wav";
-	const string grunt = "SDL2_Standardproject/Assets/sfx/grunt.wav";
-	const string wormhole = "SDL2_Standardproject/Assets/sfx/bonus.wav";
+	const string level = "../SDL2_Standardproject/Assets/sfx/musicLoop.wav";
+	const string crunch = "../SDL2_Standardproject/Assets/sfx/eating.wav";
+	const string grunt = "../SDL2_Standardproject/Assets/sfx/grunt.wav";
+	const string wormhole = "../SDL2_Standardproject/Assets/sfx/bonus.wav";
 } audio;
 
 #endif //SNAKE_PLUSPLUS_ASSETPATHS_H
